351a366a0f3723fc1f53df433cc537d0627c84da
Hassan Ali Khan <hassanalikhan87@gmail.com>
2024-07-24 09:47:06 +0500

    updated the title

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { Helmet } from "react-helmet";
 import Landing from "./screens/Landing.jsx";
 
 export default function App() {
+  document.title = "Bodacious Bulls";
   return (
     <>
       <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap"
+          rel="stylesheet"
+        />
       </Helmet>
       <Landing />
     </>
   );
 }
-
